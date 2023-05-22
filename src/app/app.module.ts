5525f15e2e53afc62e9abe514c8356919ecce5f6
Lucas Schiavo <lucas.schiavo.178@gmail.com>
2023-05-21 21:26:48 -0300

    main components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { MainCardComponent } from './components/main-card/main-card.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    MainCardComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
