484cc41cd9ef31b7dc9b64bef05e7ee9fde5ef97
Lucas Schiavo <lucas.schiavo.178@gmail.com>
2023-05-22 14:17:41 -0300

    @Input() para cards

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-card.component.css']
 })
 export class MainCardComponent {
+  @Input() thumb: string = "";
+  @Input() cardTitle: string ="";
+  @Input() cardDescription: string = "";
 
+  constructor() {}
 }
